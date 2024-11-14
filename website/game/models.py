5263b0afe2dfbc78ca5e39e6c8926cc4553a580a
DragonMZ <c0334698@online.camosun.ca>
2024-11-13 18:54:37 -0800

    tidied up and added comments

--- website/game/models.py
+++ website/game/models.py
@@ -3,11 +3,13 @@ from django.core.exceptions import ValidationError
 
 # Create your models here.
 
+#validation checker for unique name
 def validate_unique_tag(value):
     players = Player.objects.filter(tag=value)
     if len(players) != 0:
         raise ValidationError('Name already taken', code='duplicate')
 
+#the tile for the game, a row and column location and a value
 class Tile(models.Model):
     row = models.IntegerField()
     col = models.IntegerField()
@@ -21,6 +23,7 @@ class Tile(models.Model):
     def __str__(self):
         return f'{self.value}'
 
+#makes a player with a name and gives them a score of 0
 class Player(models.Model):
     name = models.CharField(max_length=16, validators=[validate_unique_tag], default='player')
     score = models.IntegerField()
